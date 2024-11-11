06d2e7019a1efe7989b6b0f1f3d111a81cb8b6a8
Aix-fone <130974790+Aix-fone@users.noreply.github.com>
2024-11-11 15:22:14 +0800

    Add files via upload

---
+++ version.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // 你可以在这里添加PHP代码
+        // 例如，定义版本号
+        $version = "1.4.20241109";
+    ?>
+    <h1>最新版本：</h1>
+    <h1><?php echo $version; ?></h1>
+</body>
+</html>
