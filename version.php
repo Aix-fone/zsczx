68215b6ab42b4a485ff203990baf23f36071f02c
Aix-fone <130974790+Aix-fone@users.noreply.github.com>
2024-11-11 15:27:10 +0800

    Update version.php

--- version.php
+++ version.php
@@ -6,12 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        // 你可以在这里添加PHP代码
-        // 例如，定义版本号
-        $version = "1.4.20241109";
-    ?>
-    <h1>最新版本：</h1>
-    <h1><?php echo $version; ?></h1>
+    <h1>最新版本：</h1>    
+    <h1>1.4.20241109</h1>
 </body>
 </html>
